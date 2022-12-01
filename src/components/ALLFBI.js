94177499a3c37d8a721827d23796918897ec3180
RWalk41 <110557479+RWalk41@users.noreply.github.com>
2022-12-01 16:52:54 -0600

    data is being pulled

--- src/components/ALLFBI.js
+++ src/components/ALLFBI.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchFBI, selectFBI } from "../store/FBISlice"
+import { fetchFBI, selectFBI, selectOri } from "../store/FBISlice"
+import { fetchStats } from "../store/CrimedataSlice";
 
  
-
-
-
-
 const ALLFBI = () => {
 
 
     const [chosenState, setChosenState] = useState("")
+    const [option, setOption ] = useState("")
+    
     const dispatch = useDispatch();
+    
     const FBIData = useSelector(selectFBI);
+    const OriData = useSelector(selectOri)
+    let Oris = []
+
+    
+    OriData.filter((ori) => ori[1] === option ?  Oris.push(ori[0]) : null) 
    
-        function onSubmit (event) {
+        
+    function onSubmit (event) {
         event.preventDefault()
+
+        dispatch(fetchStats({oris: Oris, offense: "homicide" , from: 2019, to: 2020}))
 
         dispatch(fetchFBI(chosenState));
        }
        
+
     return (
         <>
 
@@ -30,11 +39,16 @@ const ALLFBI = () => {
                 <button type = "submit" >Submit</button>
             </form>
 
-            <select>{FBIData.map((county)=><option>{county}</option>)}</select>  
+            
+           
+            <select onChange = {(evt) => setOption(evt.target.value)}><option>Select County</option>{FBIData.map((county)=><option value = {county} >{county}</option>)}</select>  
              
+            
+             
+
          </>
     )
-
+                    
     }
 export default ALLFBI
 
--- src/store/FBISlice.js
+++ src/store/FBISlice.js
@@ -4,8 +4,8 @@ import axios from "axios";
 //uMb4ZhdgjKJqrGVqx7G3DhpOWbW2YFZ36iEgxRca
 
 const initialState = {
-Counties: []
-
+Counties: [], 
+Organization: []
 
 };
 
@@ -22,26 +22,47 @@ export const fbiSlice = createSlice({
     name: "FBI",
     initialState,
     reducers: {
-        
-        
-
-
     },
     extraReducers: (builder) => {
         builder.addCase(fetchFBI.fulfilled, (state, action) => {
             const FBI = action.payload
             let MultiCounties = FBI.results.map((result) => result.county_name)
             let SingleCounties = MultiCounties.filter((item, i, County) => County.indexOf(item) === i)
+               
 
+             let OriData = FBI.results.map((result) => [result.ori, result.county_name])
+            // const OriArr = []
+            //  let OriNumber = OriData.filter((SingleCounties, index) =>OriData[index][1].includes(SingleCounties[1]) ?  OriArr.push(OriData[index][0])  : null   )
+    
+           
              state.Counties = SingleCounties
-            
-        }
+             state.Organization = OriData;
+            }
         )}
         })
 
         export const selectFBI = (state) => {
             return state.FBI.Counties;
           };
+          
+          export const selectOri = (state) => {
+
+            return state.FBI.Organization
+
+          }
 
 
 export default fbiSlice;
+
+
+
+
+
+
+// .then(result => {
+//     const topStories = result.data;
+//     const promises = topStories.map(story => {return axios.get(base + story + extension).then(res => res.data)})
+//     Promise.all(promises).then(data => {
+//     console.log(data)
+//     this.setState({ promises: data })
+//     })
