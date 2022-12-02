eb53af0370565f2062d7f826c87ac293cf6e4a6c
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2022-12-02 08:38:23 -0800

    Created Team Component

---
+++ src/components/Team.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Team() {
+  return (
+    <>
+      <div className="teamProfiles">
+        <img src="">Ryan</img>
+        <img src="">Elias</img>
+        <img src="">John</img>
+        <img src="">Ian</img>
+      </div>
+    </>
+  );
+}
+
+export default Team;
