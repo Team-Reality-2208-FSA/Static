2a2612f4a223de9dbab33467c206ddd4e5340fbf
RWalk41 <110557479+RWalk41@users.noreply.github.com>
2022-12-06 14:42:08 -0600

    state data pulled

--- src/store/CrimedataSlice.js
+++ src/store/CrimedataSlice.js
@@ -14,14 +14,14 @@ export const fetchStats = createAsyncThunk(
       const results = await axios.get(
         `https://api.usa.gov/crime/fbi/sapi/api/summarized/agencies/${ori}/${offense}/${from}/${to}?API_KEY=uMb4ZhdgjKJqrGVqx7G3DhpOWbW2YFZ36iEgxRca`
       );
-      console.log(results);
+     
       return results;
     };
 
- const arr2 = await Promise.all (oris.map( (ori) => {
+  await Promise.all (oris.map( (ori) => {
        arr.push(functionGetStats(ori, offense, from, to));
     }));
-    console.log(arr);
+   console.log(arr)
     return arr
   }
 );
--- src/store/stateDataslice.js
+++ src/store/stateDataslice.js
@@ -3,14 +3,28 @@ import axios from "axios";
 
 //uMb4ZhdgjKJqrGVqx7G3DhpOWbW2YFZ36iEgxRca
 
+
+
 const initialState = {
+    stateData: {}
 };
 
-export const fetchStateData = createAsyncThunk("fetchStateData", async (userState) =>
+export const fetchStateData = createAsyncThunk("fetchStateData", async () =>
 {
-    const {data} = await axios.get(`https://api.usa.gov/crime/fbi/sapi/api/estimates/states/${userState}}/2019/2020?API_KEY=uMb4ZhdgjKJqrGVqx7G3DhpOWbW2YFZ36iEgxRca`)
+    const stateArray = ["AL","AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA","RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
    
+
+    const getStateStats = async (state) => {
+        const {data} = await axios.get(`https://api.usa.gov/crime/fbi/sapi/api/estimates/states/${state}/2020/2020?API_KEY=uMb4ZhdgjKJqrGVqx7G3DhpOWbW2YFZ36iEgxRca`)
+
     return data
+        }
+
+    return await Promise.all (stateArray.map( (state) => {
+      return getStateStats(state);
+     }));
+    
+    
 })
 
 
@@ -24,6 +38,7 @@ export const stateDataSlice = createSlice({
         builder.addCase(fetchStateData.fulfilled, (state, action) => {
             
             state.stateData = action.payload;
+           
             }
         )}
         })
