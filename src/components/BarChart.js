0f2ec6267eafedf246adcad197bec0bc00db5509
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2022-12-07 14:29:16 -0800

    working barchart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectGraph,fetchGraphInfo } from "../store/GraphSlice";
+import { selectGraph, fetchGraphInfo } from "../store/GraphSlice";
 
 import {
   Chart as ChartJS,
@@ -22,70 +22,51 @@ ChartJS.register(
   Legend
 );
 
-
-
 function BarChart() {
-
-  
-
-
-
   const GraphData = useSelector(selectGraph);
 
   //console.log(GraphData.results)
-
 
   // const sortByYear  = () => {
   // const graph = GraphData.results;
   //  graph.sort((a, b) => { a.year - b.year })
   // }
 
-//This sorts the results by year
-const myData = [].concat(GraphData.results)
-.sort((a, b) => a.year - b.year)
+  //This sorts the results by year
+  const myData = [].concat(GraphData.results).sort((a, b) => a.year - b.year);
 
-  console.log('myData',myData)
+  // console.log("myData", myData);
 
   const [chosenState, setChosenState] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [BarValues, setBarValues] = useState([]);
-  
- 
-  
-    const  setBars  = (crime) => {
-      const arr = []
-      
-    for(const crimes of GraphData.results) {
 
-      arr.push(crimes[crime])
-      console.log(crime)
-      
+  const setBars = async (crime) => {
+    const arr = [];
 
+    for (const crimes of GraphData.results) {
+      arr.push(crimes[crime]);
+      console.log(crime);
     }
-    setBarValues(arr)
-  }
+    setBarValues(arr);
+  };
 
-  
   const labels = ["2015", "2016", "2017", "2018", "2019", "2020"];
 
-  const data =  {
+  const data = {
     labels,
 
     datasets: [
-     
       {
         label: selectedCategory,
-       
         data: BarValues,
         backgroundColor: "#333333",
       },
     ],
-  }
+  };
 
-  
-
-  
   const options = {
+    maintainAspectRation: true,
     responsive: true,
     plugins: {
       legend: {
@@ -98,59 +79,53 @@ const myData = [].concat(GraphData.results)
     },
   };
 
-
-  
-
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchGraphInfo(chosenState)); 
-  }, [chosenState]);
+    setBars(selectedCategory);
+    dispatch(fetchGraphInfo(chosenState));
+  }, [selectedCategory]);
+  // console.log(selectedCategory)
 
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(fetchGraphInfo(chosenState));
+  }
 
-  function onSubmit (event) {
-    event.preventDefault()
-  dispatch(fetchGraphInfo(chosenState));
-  setBars(selectedCategory)
-  
-   }
-
-   const handleChange = (event) => {
-    console.log(event.target.value);
+  const handleChange = (event) => {
     setSelectedCategory(event.target.value);
-
-    //setBars(selectedCategory)
-   
-   }
+    // setBars(selectedCategory);
+    dispatch(fetchGraphInfo(chosenState));
+  };
 
   return (
     <>
-    <div>
-      <form className="statsForm statsBody" onSubmit= {onSubmit}>
-            <label htmlFor = "chosenState">State initials</label>
-            <input value = {chosenState} onChange = {(evt) => setChosenState(evt.target.value)} />
-                <button type = "submit" >Submit</button>
-            </form>
-            <select onChange={handleChange}>
-              <option value = "aggravated_assault">Aggravated Assault</option>
-              <option value = "arson">Arson</option>
-              <option value = "homicide">Homicide</option>
-              <option value = "larceny">Larceny</option>
-              <option value = "motor_vehicle_theft">Vehicle Theft</option>
-              <option value = "property_crime">Property Crime</option>
-              <option value = "rape_revised">Rape</option>
-              <option value = "robbery">Robbery</option>
-              <option value = "violent_crime">Violent Crime</option>
-            </select>
+      <div className="test">
+        <form className="statsForm statsBody" onSubmit={onSubmit}>
+          <label htmlFor="chosenState">State initials</label>
+          <input
+            value={chosenState}
+            onChange={(evt) => setChosenState(evt.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <select onChange={handleChange}>
+          <option value="Select an option">Select a category</option>
+          <option value="aggravated_assault">Aggravated Assault</option>
+          <option value="arson">Arson</option>
+          <option value="homicide">Homicide</option>
+          <option value="larceny">Larceny</option>
+          <option value="motor_vehicle_theft">Vehicle Theft</option>
+          <option value="property_crime">Property Crime</option>
+          <option value="rape_revised">Rape</option>
+          <option value="robbery">Robbery</option>
+          <option value="violent_crime">Violent Crime</option>
+        </select>
 
-            <div className="barChart">
-        <Bar options={options} data={data} /> 
-      </div>
+        <Bar className="barChart" options={options} data={data} />
       </div>
     </>
   );
-  
-  }
+}
 
 export default BarChart;
