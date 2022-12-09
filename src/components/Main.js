92bba75469c1bd6b97ccc03662d23f586bc46be0
Ian <ianhaley0@gmail.com>
2022-12-09 16:11:09 -0500

    Git-sucks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ALLFBI } from "./";
+
 import { Routes, Route } from "react-router-dom";
 import Map from "./Map";
 import { Link } from "react-router-dom";
--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -4,8 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchNewsFeed, selectNewsFeed } from "../store/newsfeedSlice";
 import Card from "react-bootstrap/Card";
 
+import Card from "react-bootstrap/Card";
 
-const Newsfeed = () => {
+
+const newsfeed = () => {
   const dispatch = useDispatch();
 
   const NewsFeed = useSelector(selectNewsFeed);
@@ -19,6 +21,19 @@ const Newsfeed = () => {
     <div className="newsFeedBody">
       {NewsFeed.newsFeed
         ? NewsFeed.newsFeed.articles.map((article) => (
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={article.urlToImage} />
+              <Card.Body>
+                <Card.Title>
+                  {
+                    <a className="card-link" href={article.url}>
+                      {article.title}
+                    </a>
+                  }
+                </Card.Title>
+                <Card.Text>{article.description}</Card.Text>
+              </Card.Body>
+            </Card>
             <Card border="warning"  style={{ width: "18rem" }}>
               <Card.Img variant="top" src={article.urlToImage} />
               <Card.Body>
