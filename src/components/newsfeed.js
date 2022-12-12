d8249269e576455f4f0b059bd5d1772a71e1f866
Ian <ianhaley0@gmail.com>
2022-12-12 16:32:32 -0500

    final css cleanup

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -10,31 +10,83 @@ const newsfeed = () => {
   const dispatch = useDispatch();
 
   const NewsFeed = useSelector(selectNewsFeed);
-  console.log(NewsFeed.newsFeed ? NewsFeed.newsFeed.articles[0] : null);
+  console.log(NewsFeed.newsFeed ? NewsFeed.newsFeed.articles : null);
 
   useEffect(() => {
     dispatch(fetchNewsFeed());
   }, []);
 
   return (
+    <>
+    <h1>Crime Feed</h1>
+    <br></br>
+    <h3 className="wheelHeader">Top Stories</h3>
     <div className="newsFeedBody">
+      
       {NewsFeed.newsFeed
-        ? NewsFeed.newsFeed.articles.map((article) => (
-
-            <Card id="card-body" border="warning"  style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={article.urlToImage} />
+        ? NewsFeed.newsFeed.articles.map((article, idx) => (
+          idx < 4 && (
+            <Card id="card-body"   style={{ width: "18rem" }}>
+              <Card.Img id="card-image" variant="top" src={article.urlToImage} />
               <Card.Body>
                 <Card.Title id='card-title' >
                   { <a id="card-link" href={article.url}>{article.title}</a>}
                 </Card.Title>
                 <Card.Text id="card-text" >
                   {article.description}
+          
                   </Card.Text>
               </Card.Body>
             </Card>
-          ))
+          )
+        ))
         : null}
     </div>
+    <h3 className="wheelHeader">Your Area</h3>
+    <div className="newsFeedBody">
+      
+      {NewsFeed.newsFeed
+        ? NewsFeed.newsFeed.articles.map((article, idx) => (
+          idx > 7 && idx < 12  && (
+            <Card id="card-body"   style={{ width: "18rem" }}>
+              <Card.Img id="card-image" variant="top" src={article.urlToImage} />
+              <Card.Body>
+                <Card.Title id='card-title' >
+                  { <a id="card-link" href={article.url}>{article.title}</a>}
+                </Card.Title>
+                <Card.Text id="card-text" >
+                  {article.description}
+          
+                  </Card.Text>
+              </Card.Body>
+            </Card>
+          )
+        ))
+        : null}
+    </div>
+    <h3 className="wheelHeader">Specific Crime</h3>
+    <div className="newsFeedBody">
+      
+      {NewsFeed.newsFeed
+        ? NewsFeed.newsFeed.articles.map((article, idx) => (
+          idx > 10 && idx < 15 && (
+            <Card id="card-body"   style={{ width: "18rem" }}>
+              <Card.Img id="card-image" variant="top" src={article.urlToImage} />
+              <Card.Body>
+                <Card.Title id='card-title' >
+                  { <a id="card-link" href={article.url}>{article.title}</a>}
+                </Card.Title>
+                <Card.Text id="card-text" >
+                  {article.description}
+          
+                  </Card.Text>
+              </Card.Body>
+            </Card>
+          )
+        ))
+        : null}
+    </div>
+    </>
   );
 };
 
