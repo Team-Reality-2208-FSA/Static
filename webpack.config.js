38a0b77174efd8f9547cafb0890c2f91602a508a
Ian <ianhaley0@gmail.com>
2022-12-15 10:21:29 -0500

    V1.0

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,18 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
     ],
   },
 };
